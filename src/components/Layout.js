38140e9e0362c0a0100f36cce5517cd108188657
unknown <aleksandlario@gmail.com>
2019-12-20 20:54:47 +0000

    Font awesome import back to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,11 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import "../styles/index.scss"
 import layoutStyles from "./Layout.module.scss"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+// Prevent fontawesome from dynamically adding its css
+config.autoAddCss = false
+import "../fontawesome"
 
 const Layout = ({ children }) => (
   <>
