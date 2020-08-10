03d962ee77bb678385139ff01256ca3447578995
unknown <aleksandlario@gmail.com>
2020-08-10 15:51:10 +0100

    Add resume

--- src/content/summary.js
+++ src/content/summary.js
@@ -9,8 +9,8 @@ export default [
       '["<a href="mailto:aleksandlario@gmail.com">aleksandlario@gmail.com</a>", "<a rel="noopener" target="_blank" href="https://www.linkedin.com/in/aleksandrs-larionovs/">LinkedIn</a>", "<a rel="noopener" target="_blank" href="https://github.com/alekslario">github</a>"]',
   },
   {
-    input: "Alex.weakness",
-    return: `"Tabasco pepper"`,
+    input: "Alex.resume",
+    return: `['<a href="AleksandrsLarionovsResume.pdf" download >resume.pdf</a>']`,
   },
   {
     input: "Alex.interests",
